df84410e1d5e5eb38cf04628091fdad2edc80171
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-29 01:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn=document.getElementById("incrementBtn");
+let count=0;
+btn.addEventListener("click",()=>{
+	alert(count);
+	count=count+1;
+	let para=document.getElementById("counter");
+	
+	para.innerText=count;
+})
